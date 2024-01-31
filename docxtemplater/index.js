ba28fd6f6c7a4fbcec8ed09ef7b87dbac0130d97
tau-kappa <158305167+tau-kappa@users.noreply.github.com>
2024-01-31 19:46:36 +0100

    Create index.js

---
+++ docxtemplater/index.js
@@ -0,0 +1,78 @@
+"use strict";
+
+function ready(fn) {
+  if (document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+const ATTACHMENTID_COL_NAME = "attachment_id";
+const DATA_COL_NAME = "data";
+const FILENAME_COL_NAME = "filename";
+const currentData = { url: null, data: null, outputFileName: null, };
+const elem_status = document.querySelector("#status");
+
+function handleError(err) {
+  elem_status.innerHTML = String(err);
+}
+
+async function gristRecordSelected(record, mappedColNamesToRealColNames) {
+  const mappedRecord = grist.mapColumnNames(record);
+  if (mappedRecord) {
+    try {
+      let attachmentIt = mappedRecord[ATTACHMENTID_COL_NAME];
+      let const tokenInfo = await grist.docApi.getAccessToken({ readOnly: true });
+      currentData.url = `${tokenInfo.baseUrl}/attachments/${data.input.attachmentId}/download?auth=${tokenInfo.token}`;
+      currentData.data = mappedRecord[DATA_COL_NAME];
+      currentData.outputFileName = mappedRecord[FILENAME_COL_NAME];
+    } catch (err) {
+      handleError(err);
+    }
+  }
+}
+
+async function processFile(url, data, outputFileName) {
+  try {
+    if (!url || !data || !outputFileName) {
+      throw new Error("Any of the arguments 'url', 'data', 'outputFileName' seems to be missing/falsy.");
+    }
+    return PizZipUtils.getBinaryContent(url, function(err, content) {
+      if (err) {
+        throw err;
+      }
+      const templater = docxtemplater(new PizZip(content), {
+        paragraphLoop: true,
+        linebreaks: true,
+      });
+      templater.render(data);
+      saveAs(templater.getZip().generate({
+        type: "blob",
+        mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        compression: "DEFLATE",
+      }), outputFileName);
+    });
+  } catch (err) {
+    handleError(err);
+  }
+}
+
+
+
+
+
+ready(async function(){
+  grist.ready({
+    requiredAccess: "read",
+    columns: [
+      { name: ATTACHMENTID_COL_NAME, title: "Attachment ID", description: "ID number of a Grist attachment." },
+      { name: DATA_COL_NAME, title: "Placeholder Data", description: "Must be a dictionary of the form {placeholder_name: value_to_replace_by}" },
+      { name: FILENAME_COL_NAME, title: "Output File Name", description: "Name of the resulting file that will be offered for download. Should include the '.docx' extension." },
+    ],
+  });
+  grist.onRecord(gristRecordSelected);
+  document.querySelector("#button_process").addEventListener("click", function(){
+    processFile(currentData.url, currentData.data, currentData.outputFileName);
+  });
+});
