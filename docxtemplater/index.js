fae87bda5208580d876d211f37572f61c1374f11
tau-kappa <158305167+tau-kappa@users.noreply.github.com>
2024-01-31 20:17:02 +0100

    Update index.js

--- docxtemplater/index.js
+++ docxtemplater/index.js
@@ -62,7 +62,7 @@ async function processFile(url, data, outputFileName) {
 
 ready(async function(){
   grist.ready({
-    requiredAccess: "read",
+    requiredAccess: "read table",
     columns: [
       { name: ATTACHMENTID_COL_NAME, title: "Attachment ID", description: "ID number of a Grist attachment." },
       { name: DATA_COL_NAME, title: "Placeholder Data", description: "Must be a dictionary of the form {placeholder_name: value_to_replace_by}" },
