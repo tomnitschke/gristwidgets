a136096d691969ec04b41c6a1e8470278de63652
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2024-02-29 13:11:32 +0100

    Create index.js

---
+++ viewerjs/index.js
@@ -0,0 +1,84 @@
+function ready(fn) {
+  if (document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+const ATTACHMENTID_COL_NAME = "attachment_id";
+let gristAccessToken = null;
+
+function setStatusMessage(msg) {
+  let statusMessageElem = document.querySelector("#status");
+  if (!statusMessageElem) return false;
+  statusMessageElem.innerHTML = msg;
+  return true;
+}
+
+function resetStatusMessage() {
+  return setStatusMessage("Loading...");
+}
+
+function handleError(err) {
+  if (!setStatusMessage(err)) {
+    console.error("viewerjs: FATAL: ", err);
+    document.body.innerHTML = String(err);
+    return;
+  }
+  console.error("viewerjs: ", err);
+}
+
+async function gristGetAttachmentURL(attachmentId) {
+  if (!(/^\d+$/.test(attachmentId))) {
+    let msg = `Invalid Grist attachment id '${attachmentId}'. It should be a number but is of type '${typeof attachmentId}'.`;
+    console.error(`viewerjs: ${msg}`);
+    throw new Error(msg);
+  }
+  attachmentId = Number(attachmentId);
+  // Get a Grist access token if we don't already have one.
+  if (!gristAccessToken) {
+    console.log(`viewerjs: Getting new Grist access token.`);
+    gristAccessToken = await grist.docApi.getAccessToken({ readOnly: true });
+  }
+  // Use the token to get a URL to the attachment.
+  let url = `${gristAccessToken.baseUrl}/attachments/${attachmentId}/download?auth=${gristAccessToken.token}`;
+  console.log(`viewerjs: Obtained Grist attachment URL: '${url}'`);
+  return url;
+}
+
+async function gristRecordSelected(record, mappedColNamesToRealColNames) {
+  console.log("viewerjs: gristRecordSelected() with record, mappedColNamesToRealColNames:", record, mappedColNamesToRealColNames);
+  resetStatusMessage();
+  try {
+    const mappedRecord = grist.mapColumnNames(record);
+    if (!mappedRecord) {
+      throw new Error("Please map all required columns first.");
+    }
+    // Get the URL we want to view.
+    let url = await gristGetAttachmentURL(mappedRecord[ATTACHMENTID_COL_NAME]);
+    // Redirect to ViewerJS to view the file.
+    window.location.replace(`${window.location.href}/ViewerJS/#${url}`);
+  } catch(err) {
+    return handleError(err);
+  }
+}
+
+// Start once the DOM is ready.
+ready(function(){
+  // Set up a global error handler.
+  window.addEventListener("error", function(err) {
+    handleError(err);
+  });
+  // Let Grist know we're ready to talk.
+  grist.ready({
+    // We require "full" mode in order to be allowed access to attachments.
+    requiredAccess: "full",
+    columns: [
+      { name: ATTACHMENTID_COL_NAME, type: "Int", title: "Attachment ID", description: "ID number of a Grist attachment." },
+    ],
+  });
+  // Register callback for when the user selects a record in Grist.
+  grist.onRecord(gristRecordSelected);
+  console.log("viewerjs: Ready.");
+});
