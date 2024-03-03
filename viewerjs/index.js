c4dda52d0988cf7bc391edb39d2251ca8b3598eb
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2024-03-03 17:44:03 +0100

    Update index.js

--- viewerjs/index.js
+++ viewerjs/index.js
@@ -40,7 +40,7 @@ function handleError(err) {
 
 async function gristGetAttachmentURL(attachmentId) {
   if (!(/^\d+$/.test(attachmentId))) {
-    let msg = `Invalid Grist attachment id '${attachmentId}'. It should be a number but is of type '${typeof attachmentId}'.`;
+    let msg = `Invalid Grist attachment id '${attachmentId}'. It should be an integer but is of type '${typeof attachmentId}'.`;
     console.error(`viewerjs: ${msg}`);
     throw new Error(msg);
   }
