3d4a45da12e1cca86a8f9e83f9cca3fdec23fa15
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2024-03-04 13:13:36 +0100

    Update index.js

--- spreadsheet/index.js
+++ spreadsheet/index.js
@@ -36,43 +36,6 @@ function handleError(err) {
   console.error("spreadsheet: ", err);
 }
 
-async function gristGetAttachmentURL(attachmentId) {
-  if (!(/^\d+$/.test(attachmentId))) {
-    let msg = `Invalid Grist attachment id '${attachmentId}'. It should be an integer but is of type '${typeof attachmentId}'.`;
-    console.error(`spreadsheet: ${msg}`);
-    throw new Error(msg);
-  }
-  attachmentId = Number(attachmentId);
-  // Get a Grist access token if we don't already have one.
-  if (!gristAccessToken) {
-    console.log(`spreadsheet: Getting new Grist access token.`);
-    gristAccessToken = await grist.docApi.getAccessToken({ readOnly: true });
-  }
-  // Use the token to get a URL to the attachment.
-  let url = `${gristAccessToken.baseUrl}/attachments/${attachmentId}/download?auth=${gristAccessToken.token}`;
-  console.log(`spreadsheet: Obtained Grist attachment URL: '${url}'`);
-  return url;
-}
-
-async function imageGetBase64Url(url) {
-  let response = await fetch(url);
-  let blob = await response.blob();
-  const reader = new FileReader();
-  return new Promise(function(resolve, reject) {
-    reader.onerror = reject;
-    reader.onloadend = function() {
-      resolve(reader.result);
-    };
-    reader.readAsDataURL(blob);
-  });
-}
-
-/*async function imageGetBlobUrl(url) {
-  let response = await fetch(url);
-  let blob = await response.blob();
-  return URL.createObjectURL(blob);
-}*/
-
 async function gristRecordSelected(record, mappedColNamesToRealColNames) {
   console.log("spreadsheet: gristRecordSelected() with record, mappedColNamesToRealColNames:", record, mappedColNamesToRealColNames);
   setStatus("Loading...");
