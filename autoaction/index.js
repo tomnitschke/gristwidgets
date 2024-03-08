7ac02c5bf841fa3e602a364bb79f7bc6e29b0df1
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2024-03-08 10:28:25 +0100

    Make one-shot setting configurable

--- autoaction/index.js
+++ autoaction/index.js
@@ -8,6 +8,8 @@ function ready(fn) {
 
 const ACTIONS_COL_NAME = "actions";
 const ISENABLED_COL_NAME = "isenabled";
+const ISONESHOT_COL_NAME = "isoneshot";
+
 let isDoneForRecord = [];
 const ACTIONS_FORMAT_EXAMPLE_FORMULA = `<pre>return [
   # The 'UpdateRecord' action takes the parameters: 'table_name' (str), 'record_id' (int), 'data' (dict, like { 'column_name': 'value_to_update_to' })
@@ -65,7 +67,11 @@ async function gristRecordSelected(record, mappedColNamesToRealColNames) {
       setStatus(`List of actions seems invalid. It needs to be a list of lists, so your column formula needs to look similar to this:<br />${ACTIONS_FORMAT_EXAMPLE_FORMULA}`);
       return;
     }
-    if (isDoneForRecord.includes(record.id)) {
+    let isOneShot = false;
+    if (ISONESHOT_COL_NAME in mappedRecord) {
+      isOneShot = mappedRecord[ISONESHOT_COL_NAME];
+    }
+    if (!isOneShot && isDoneForRecord.includes(record.id)) {
       // If we've already executed actions for this record, provide a message to that extent and quit.
       let msg = `Already executed actions for this record (ID ${record.id}), won't do it again until the page gets reloaded.`;
       setStatus(msg);
@@ -79,6 +85,7 @@ async function gristRecordSelected(record, mappedColNamesToRealColNames) {
     }
     // Apply the user actions.
     // Set 'isDone' for this record *first*, so we're safe even if the applyUserActions() call somehow screws up.
+    // Note that this safeguard will be ignored if the user set the "one-shot" option to false.
     isDoneForRecord.push(record.id);
     console.log("autoaction: Applying actions:", actions);
     await grist.docApi.applyUserActions(actions);
@@ -105,6 +112,7 @@ ready(function(){
     columns: [
       { name: ACTIONS_COL_NAME, type: "Any", strictType: true, title: "Actions", description: "List of user actions to execute. As each user action definition is a list, this column must hold a list of lists. See https://github.com/gristlabs/grist-core/blob/main/documentation/overview.md#changes-to-documents" },
       { name: ISENABLED_COL_NAME, type: "Bool", title: "Enabled?", description: "If this column's value is False, the widget won't do anything." },
+      { name: ISONESHOT_COL_NAME, type: "bool", title: "One-shot?", description: "If this is True, actions will be executed just once per record (until the page gets reloaded). The default is True." },
     ],
   });
   // Register callback for when the user selects a record in Grist.
