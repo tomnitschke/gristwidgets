06aa500e2fa9577107661e210e31dda6cfcb322a
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2024-09-04 14:20:14 +0200

    Update index.js

--- documentize/index.js
+++ documentize/index.js
@@ -217,7 +217,7 @@ function processData() {
   try {
     if (format == "docx") {
       $(document).googoose(currentData.config_docx);
-    } else if (format == "pdf") {
+    } else {
       let docElem = document.querySelector("#document");
       let pagebreakElements = docElem.querySelectorAll(currentData.config_pdf.pagebreak.after);
       for (const pagebreakElem of pagebreakElements) {
@@ -228,13 +228,6 @@ function processData() {
           pagebreakElem.style.display = "revert";
         }
       });
-    } else {
-      // Process HTML->PDF using the jsPDF library.
-      // Default format is a4 paper, portrait, using millimeters for units
-      const doc = new jsPDF();
-      doc.html(document.querySelector("#document"), function(doc) {
-        doc.save(currentData.filename);
-      })
     }
     console.log("documentize: Processing done. Offering up the file for download!");
   } catch (err) {
@@ -245,8 +238,6 @@ function processData() {
 
 // Start once the DOM is ready.
 ready(function(){
-  // Import jsPDF.
-  //window.jsPDF = window.jspdf.jsPDF;
   // Set up a global error handler.
   window.addEventListener("error", function(err) {
     handleError(err);
