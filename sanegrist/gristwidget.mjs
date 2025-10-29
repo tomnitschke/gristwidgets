39a880ddb45d1d96cc5e4c3127d380bba71ee992
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-10-29 11:48:32 +0100

    Update gristwidget.mjs

--- sanegrist/gristwidget.mjs
+++ sanegrist/gristwidget.mjs
@@ -23,7 +23,7 @@ export class GristWidget extends EventTarget {
     if (!this.eventControl.onRecords.wasEverTriggered) {
       this.eventControl.onRecords.wasEverTriggered = true;
       this.#updateColMappings(colMappings, true); this.#updateRecords(records, true);
-      if (!this.wasReadyEventDispatched && this.eventControl.onRecord.wasEverTriggered /*&& this.cursor.current?.id*/) { Util.log("dispatching ready-event from onRecords"); 
+      if (!this.wasReadyEventDispatched && this.eventControl.onRecord.wasEverTriggered /*&& this.cursor.current?.id*/) { Util.log("dispatching ready-event from onRecords",this.records,this.cursor,this.colMappings); 
         this.wasReadyEventDispatched = true; this.dispatchEvent(new GristWidget.ReadyEvent(this.records.current, this.cursor.current, this.colMappings.current)); }
       return;
     }
@@ -38,7 +38,7 @@ export class GristWidget extends EventTarget {
     if (!this.eventControl.onRecord.wasEverTriggered) {
       this.eventControl.onRecord.wasEverTriggered = true;
       this.#updateColMappings(colMappings, true); this.#updateCursor(record, true);
-      if (!this.wasReadyEventDispatched && this.eventControl.onRecords.wasEverTriggered) { Util.log("dispatching ready-event from onRecord");
+      if (!this.wasReadyEventDispatched && this.eventControl.onRecords.wasEverTriggered) { Util.log("dispatching ready-event from onRecord",this.records,this.cursor,this.colMappings);
         this.wasReadyEventDispatched = true; this.dispatchEvent(new GristWidget.ReadyEvent(this.records.current, this.cursor.current, this.colMappings.current));
       }
       return;
@@ -54,7 +54,7 @@ export class GristWidget extends EventTarget {
     if (!this.eventControl.onNewRecord.wasEverTriggered) {
       this.eventControl.onNewRecord.wasEverTriggered = true;
       this.#updateColMappings(colMappings, true); this.#updateCursor(undefined, true);
-      if (!this.wasReadyEventDispatched && this.eventControl.onRecords.wasEverTriggered) { Util.log("dispatching ready-event from onNewRecord");
+      if (!this.wasReadyEventDispatched && this.eventControl.onRecords.wasEverTriggered) { Util.log("dispatching ready-event from onNewRecord",this.records,this.cursor,this.colMappings);
         this.wasReadyEventDispatched = true; this.dispatchEvent(new GristWidget.ReadyEvent(this.records.current, this.cursor.current, this.colMappings.current));
       }
       return;
