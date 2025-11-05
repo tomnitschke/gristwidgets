23da688721eda3046d49edc332bd778d6d765e8c
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-11-05 20:25:16 +0100

    Update gristwidget.mjs

--- sanegrist/gristwidget.mjs
+++ sanegrist/gristwidget.mjs
@@ -217,8 +217,8 @@ export class GristWidget extends EventTarget {
     if (existingScheduledOp) {
       window.clearTimeout(this.#recordOps[key].timeoutHandle);
       delete this.#recordOps[key];
-      if (runLastScheduledIfDue && existingScheduledOp.timeScheduled + existingScheduledOp.timeoutMs >= now) {
-        this.debug("running last scheduled op for recId",recId,"as it was scheduled",existingScheduledOp.timeScheduled,"with a timeout of",existingScheduledOp.timeoutMs,"and that's >= now:",now);
+      if (runLastScheduledIfDue && now - existingScheduledOp.timeScheduled >= timeoutMs) {
+        this.debug("running last scheduled op for recId",recId,"as it was scheduled",existingScheduledOp.timeScheduled,"with a timeout of",existingScheduledOp.timeoutMs,"and that's >= now:",now,"===",now-existingScheduledOp.timeScheduled);
         existingScheduledOp.fn();
       }
     }
