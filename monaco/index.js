f893618938bf3c01d19eebad2086084d3896a466
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-11-06 11:33:02 +0100

    Update index.js

--- monaco/index.js
+++ monaco/index.js
@@ -3,15 +3,33 @@
 import { GristWidget, Util } from 'https://tomnitschke.github.io/gristwidgets/sanegrist/gristwidget.mjs';
 import MonacoLoader from 'https://esm.sh/@monaco-editor/loader@1.6.1';
 
+Util.onDOMReady(() => {
+  const gristMonaco = new GristMonaco();
+});
+
+
+/*****************************************************************************************************/
+Config = {
+  autosaveTimeoutMs: 500,
+  defaultCodeLang: 'javascript',
+}
+
+
+/*****************************************************************************************************/
 class GristMonaco {
-  constructor () {
+  constructor (config=null) {
     this.widget = new GristWidget('GristMonaco', {
       requiredAccess: 'full',
       columns: [
         { name: 'content', title: 'Content', type: 'Text', strictType: true },
+        { name: 'codeLang', optional: true, title: 'Code Language', type: 'Text', description: `Used for syntax highlighting and autocomplete. For available languages, see  Defaults to '${this.config.defaultCodeLang}'` },
       ],
     }, true);
     this.debug = this.widget.logger.debug.bind(this.widget.logger);
+    this.config = {
+      ...Config,
+      ...config,
+    };
     this.api = null;
     this.editor = null;
     this.editorModel = null;
@@ -31,29 +49,25 @@ class GristMonaco {
       lineNumbers: 'on',
       folding: true,
     });
-    this.editor.onDidChangeModelContent((evt) => {
-      //this.debug("onDidChangeModelContent",evt,"current content:",this.editorModel.getValue());
-      this.widget.scheduleWriteRecord({
-        [this.widget.colMappings.current.content]: this.editorModel.getValue(),
-      }, 2000);
-    });
-    this.debug("monaco loaded:",this.editor);
+    this.editor.onDidChangeModelContent(this.onDidChangeModelContent.bind(this));
+    this.debug("monaco loaded:",this.editor,this.api.languages.getLanguages());
     this.#setEditorContent();
   }
   async load (content) {
     this.debug("load",content);
     this.#setEditorContent(content);
   }
-  #setEditorContent (content=undefined, language=undefined, modelOptions=null) {
-    this.editorModel = this.api.editor.createModel(content || '', language || 'javascript');
+  #onDidChangeModelContent (evt) {
+    this.widget.scheduleWriteRecord({
+      [this.widget.colMappings.current.content]: this.editorModel.getValue(),
+    }, this.config.autosaveTimeoutMs);
+  }
+  #setEditorContent (content=undefined, codeLang=undefined, modelOptions=null) {
+    this.editorModel = this.api.editor.createModel(content || '', codeLang || this.config.defaultCodeLang);
     this.editor.setModel(this.editorModel);
     this.editorModel.updateOptions({ tabSize: 3, ...modelOptions });
   }
 }
-
-Util.onDOMReady(() => {
-  const gristMonaco = new GristMonaco();
-});
 
 
 
