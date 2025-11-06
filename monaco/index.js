94882716d3dbc52a5b83828fb82415da88c5e999
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-11-06 14:25:08 +0100

    i'm going nuts

--- monaco/index.js
+++ monaco/index.js
@@ -34,15 +34,15 @@ class GristMonaco {
     }, true);
     this.debug = this.widget.logger.debug.bind(this.widget.logger);
     this.eContainer = document.querySelector('#monaco'); this.eConfigPanel = document.querySelector('#config'); /*this.eConfigSaveBtn = document.querySelector('#configSaveBtn');*/
+    for (const eConfigItem of document.querySelectorAll('.configItem')) {
+      eConfigItem.addEventListener('sl-input', async (evt) => await this.#onConfigItemChanged(evt.target));
+    }
     //this.eConfigSaveBtn.addEventListener('click', async () => await this.commitConfigPanel());
     this.widget.addEventListener('ready', async (evt) => { await this.init(); await this.load(evt.cursor?.[evt.colMappings.content]); });
     this.widget.addEventListener('cursorMoved', async (evt) => await this.load(evt.cursor?.[evt.colMappings.content]));
     this.widget.addEventListener('optionsEditorOpened', async () => await this.openConfigPanel());
   }
   async init () {
-    for (const eConfigItem of document.querySelectorAll('.configItem')) {
-      eConfigItem.addEventListener('sl-input', async (evt) => this.#onConfigItemChanged(evt.target));
-    }
     this.api = await MonacoLoader.init();
     this.editor = this.api.editor.create(this.eContainer, {
       model: this.editorModel,
@@ -64,7 +64,7 @@ class GristMonaco {
     const configKey = eConfigItem.id.slice(7);
     let value = eConfigItem.value;
     if (eConfigItem.tagName === 'sl-checkbox') { value = Boolean(eConfigItem.checked); }
-    else if (eConfigItem.type === 'number') { value = isNaN(eConfigItem.valueAsNumber) ? 0 : eConfigItem.valueAsNumber;
+    else if (eConfigItem.type === 'number') { value = isNaN(eConfigItem.valueAsNumber) ? 0 : eConfigItem.valueAsNumber; }
     this.debug("save config item", configKey, eConfigItem, value, typeof value);
   }
   async #getConfigElements () {
