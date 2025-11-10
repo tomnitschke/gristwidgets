6079c5873d38bf5626dc1094aae4756d48918521
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-11-10 17:47:25 +0100

    Update gristwidget.mjs

--- sanegrist/gristwidget.mjs
+++ sanegrist/gristwidget.mjs
@@ -198,5 +198,5 @@ export class GristWidget extends EventTarget {
   }
   get currentRecId () { return this.cursor.current?.id; }
   get prevRecId () { return this.cursor.prev?.id; }
-  isColMapped (mappedColName) { return Boolean(mappedColname in this.colMappings.current); }
+  isColMapped (mappedColName) { return Boolean(mappedColName in this.colMappings.current); }
 };
