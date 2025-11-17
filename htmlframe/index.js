5c006ddace4155e4012f7d307cc1e1ac0a0e0a5d
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-11-17 16:37:13 +0100

    Update index.js

--- htmlframe/index.js
+++ htmlframe/index.js
@@ -29,7 +29,6 @@ class GristHTMLFrame {
       //this.err("Error in js fetched from Grist record:", error);
       //return true;
     //}
-                                                  /*this.debug = (...args) => { console.info(...args); };*/
     this.eContentFrame = document.querySelector('#content');
     this.eContentDocument = this.eContentFrame.contentWindow.document;
     this.#readyMessageTimeoutHandler = undefined;
@@ -54,92 +53,15 @@ class GristHTMLFrame {
         this.eContentFrame.contentWindow.postMessage(msg.data, '*');
       }
     });
-                                        //grist.sectionApi.configure(this.widget.gristOptions);
-                                        this.#readyMessageTimeoutHandler = setTimeout(() => { grist.sectionApi.configure(this.widget.gristOptions); }, 1000);
-    //this.eContentFrame.contentWindow.grist = grist;
-    /*grist.rpc.sendReadyMessage();
-    grist.rpc.registerFunc('editOptions', () => {});
-    window.addEventListener('message', (msg) => {
-      if (msg.source === this.eContentFrame.contentWindow) {
-        if (msg.data?.iface === 'CustomSectionAPI' && msg.data?.meth === 'configure') {
-          this.debug("MSG:",msg,"data:",structuredClone(msg.data));
-          msg.data.args ??= [{}];
-          msg.data.args[0].hasCustomOptions = true;
-          msg.data.args[0].columns = [...(msg.data.args[0].columns || []), { name: 'added', type: 'Bool' }];
-        }
-        window.parent.postMessage(msg.data, '*');
-      } else if (msg.source === window.parent) {
-        this.eContentFrame.contentWindow.postMessage(msg.data, '*');
-      }
-    });
-    this.eContentDocument.body.innerHTML = `
-<html>
-<head>
-  <style>
-    html, body { margin:0; padding:0; }
-    #datagrid { border:solid 10px red; box-sizing:border-box; }
-  </style>
-</head>
-<body>
-  <div id="datagrid"></div>
-</body>
-</html>
-      `;
-    const eScript = this.eContentDocument.createElement('script');
-    eScript.src = 'https://docs.getgrist.com/grist-plugin-api.js';
-    eScript.defer = false;
-    eScript.async = false;
-    eScript.addEventListener('load', () => {
-      const eScript2 = this.eContentDocument.createElement('script');
-      eScript2.type = 'module';
-      eScript2.innerHTML = `
-  import { GristWidget } from 'https://tomnitschke.github.io/gristwidgets/sanegrist/gristwidget.mjs';
-  import { Util } from 'https://tomnitschke.github.io/gristwidgets/sanegrist/util.mjs'
-  import canvasDatagrid from 'https://esm.sh/canvas-datagrid@0.4.7/?dev';
-  
-  class GristCanvasGrid {
-      constructor (eContainer) {
-          this.eContainer = eContainer;
-          this.widget = new GristWidget('GristCanvasGrid', {
-              requiredAccess: 'full',
-              allowSelectBy: true,
-              columns: [
-                  { name: 'test', type: 'Any' },
-              ],
-          }, true);
-          this.debug = this.widget.logger.debug.bind(this.widget.logger);
-          this.debug(window);//
-          this.msg = this.widget.logger.msg.bind(this.widget.logger);
-          this.err = this.widget.logger.err.bind(this.widget.logger);
-          this.widget.addEventListener('cursorMoved', (evt) => console.log("cursorMoved",evt));
-          this.widget.addEventListener('cursorMovedToNew', (evt) => console.log("cursorMovedToNew",evt));
-          this.widget.addEventListener('ready', (readyEvent) => {
-              this.debug("INIT");
-              this.grid = canvasDatagrid();
-              this.eContainer.innerHTML = '';
-              this.eContainer.appendChild(this.grid);
-              this.grid.data = [
-                  { col1: 'row 1 column 1', col2: 'row 1 column 2', col3: 'row 1 column 3' },
-                  { col1: 'row 2 column 1', col2: 'row 2 column 2', col3: 'row 2 column 3' },
-              ];
-          });
-      }
-  }
-  
-  Util.onDOMReady(() => {
-      const gristCanvasGrid = new GristCanvasGrid(document.querySelector('#datagrid'));
-  });
-        `;
-      this.eContentDocument.body.appendChild(eScript2);
-    });
-    this.eContentDocument.body.appendChild(eScript);*/
+    this.#readyMessageTimeoutHandler = setTimeout(() => { grist.sectionApi.configure(this.widget.gristOptions); }, 1000);
   }
   load (record) {
-    /*if (this.widget.isColMapped('html')) {
+    this.eContentDocument.body.innerHTML = '';
+    if (this.widget.isColMapped('html')) {
       this.eContentDocument.body.innerHTML = record[this.widget.colMappings.current.html];
-    }*/
-    this.eContentDocument.body.innerHTML = record[this.config.colNameForHtml];
-    //if (this.widget.isColMapped('js')) {
+    }
+    //this.eContentDocument.body.innerHTML = record[this.config.colNameForHtml];
+    if (this.widget.isColMapped('js')) {
       const eGristPluginApiScript = this.eContentDocument.createElement('script');
       eGristPluginApiScript.src = 'https://docs.getgrist.com/grist-plugin-api.js';
       eGristPluginApiScript.defer = false;
@@ -147,14 +69,11 @@ class GristHTMLFrame {
       eGristPluginApiScript.addEventListener('load', () => {
         const eCustomScript = this.eContentDocument.createElement('script');
         eCustomScript.type = 'module';
-        //eCustomScript.innerHTML = record[this.widget.colMappings.current.js];
-        eCustomScript.innerHTML = record[this.config.colNameForJs];
+        eCustomScript.innerHTML = record[this.widget.colMappings.current.js];
+        //eCustomScript.innerHTML = record[this.config.colNameForJs];
         this.eContentDocument.body.appendChild(eCustomScript);
       });
       this.eContentDocument.body.appendChild(eGristPluginApiScript)
-    //}
-    if (typeof this.#readyMessageTimeoutHandler === 'undefined') {
-      //this.#readyMessageTimeoutHandler = setTimeout(() => { grist.sectionApi.configure(this.widget.gristOptions); }, 500);
     }
   }
 }
