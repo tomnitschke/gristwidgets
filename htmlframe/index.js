a2cdc6b1e2fb3666f5e6a8f948c671e504a69fbe
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-11-17 11:55:13 +0100

    Update index.js

--- htmlframe/index.js
+++ htmlframe/index.js
@@ -34,6 +34,59 @@ class GristHTMLFrame {
       }
       window.parent.postMessage(msg.data, '*');
     });
+    this.eContentDocument.body.innerHTML = ```
+<html>
+<head>
+  <style>
+    html, body { margin:0; padding:0; }
+    #datagrid { border:solid 10px red; box-sizing:border-box; }
+  </style>
+</head>
+<body>
+  <div id="datagrid"></div>
+</body>
+</html>
+      ```;
+    const eScript2 = this.eContentDocument.createElement('script');
+    eScript2.type = 'module';
+    eScript2.innerHTML = ```
+import { GristWidget } from 'https://tomnitschke.github.io/gristwidgets/sanegrist/gristwidget.mjs';
+import { Util } from 'https://tomnitschke.github.io/gristwidgets/sanegrist/util.mjs'
+import canvasDatagrid from 'https://esm.sh/canvas-datagrid@0.4.7/?dev';
+
+class GristCanvasGrid {
+    constructor (eContainer) {
+        this.eContainer = eContainer;
+        this.widget = new GristWidget('GristCanvasGrid', {
+            requiredAccess: 'full',
+            allowSelectBy: true,
+            columns: [
+            ],
+        }, true);
+        this.debug = this.widget.logger.debug.bind(this.widget.logger);
+        this.debug(window);//
+        this.msg = this.widget.logger.msg.bind(this.widget.logger);
+        this.err = this.widget.logger.err.bind(this.widget.logger);
+        this.widget.addEventListener('cursorMoved', (evt) => console.log("cursorMoved",evt));
+        this.widget.addEventListener('cursorMovedToNew', (evt) => console.log("cursorMovedToNew",evt));
+        this.widget.addEventListener('ready', (readyEvent) => {
+            this.debug("INIT");
+            this.grid = canvasDatagrid();
+            this.eContainer.innerHTML = '';
+            this.eContainer.appendChild(this.grid);
+            this.grid.data = [
+                { col1: 'row 1 column 1', col2: 'row 1 column 2', col3: 'row 1 column 3' },
+                { col1: 'row 2 column 1', col2: 'row 2 column 2', col3: 'row 2 column 3' },
+            ];
+        });
+    }
+}
+
+Util.onDOMReady(() => {
+    const gristCanvasGrid = new GristCanvasGrid(document.querySelector('#datagrid'));
+});
+      ```;
+    this.eContentDocument.body.appendChild(eScript2);
   }
   load (record) {
     if (this.widget.isColMapped('html')) {
