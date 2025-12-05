50ce9ccc6070a9fd20e0ff4ac358dba559b265e5
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-12-05 13:56:50 +0100

    oh ffs

--- sandbox/index.js
+++ sandbox/index.js
@@ -87,7 +87,7 @@ class GristSandbox {
         }
         if (this.config.importGristThemeCSSVars && (jsContent || htmlContent)) {
           this.eContentDocument.document.body.appendChild(
-            document.importNode(parent.document.querySelector('style#grist-theme'), true));
+            document.importNode(parent.document.querySelector('style#grist-theme'), true);
           );
         }
       });
