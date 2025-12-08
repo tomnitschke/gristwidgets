10a2487c66c1d54d598f395223237aa42df5f0cf
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2025-12-08 16:41:51 +0100

    Update index.js

--- playground/index.js
+++ playground/index.js
@@ -169,7 +169,7 @@ class GristPlayground {
       await grist.sectionApi.configure(this.adapter.readyPayload);
       return;
     }
-    if (!this.adapter.tableName) {
+    if (!this.adapter.tableName) {  // This information will be missing because we've disabled GristSectionAdapter's init event functionality, see ctor.
         [this.adapter.tableName, this.adapter.tableOps = await Promise.all([
           await grist.getSelectedTableId(),
           await grist.getTable()
